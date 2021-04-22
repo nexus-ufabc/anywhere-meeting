b1523842df4363d34275ef930b739a4254cebc5a
Daniel D'Angelo Resende Barros <ddangelorb@outlook.com>
2021-04-22 20:36:10 -0300

    handling microphone permission

--- src/prof-aovivo/script.js
+++ src/prof-aovivo/script.js
@@ -1,3 +1,11 @@
+navigator.mediaDevices.getUserMedia({ audio: true })
+.then(function(stream) {
+  console.log('You let me use your mic!')
+})
+.catch(function(err) {
+  console.log('No mic for you!')
+});
+
 var speechRecon = new webkitSpeechRecognition();
 speechRecon.continuous = true;
 speechRecon.interimResults = true;
